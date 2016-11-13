6f69e271e84ee1e9caebaa348374c5f2631f293f
tomard <tomard@usi.ch>
2016-11-13 03:31:25 +0100

    all convolutions

--- cifar-10/shuffle/CIFAR-10-random_shuffle.py
+++ cifar-10/shuffle/CIFAR-10-random_shuffle.py
@@ -1,9 +1,10 @@
-
 # coding: utf-8
 import cPickle
 import numpy as np
 import tensorflow as tf
-from heapq import nlargest,nsmallest
+import tflearn
+from heapq import nlargest, nsmallest
+
 
 def unpickle(file):
     fo = open(file, 'rb')
@@ -11,129 +12,58 @@ def unpickle(file):
     fo.close()
     return dict_
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
 
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-def conv2d(x, W):
-    return tf.nn.relu(tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME'))
-
-def max_pool_3x3(x):
-    return tf.nn.max_pool(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-
-batch_size = 100
+batch_size = 128
 
 graph = tf.Graph()
 with graph.as_default():
-    x = tf.placeholder(tf.float32, [batch_size, 3072])
-    y = tf.placeholder(tf.float32, [batch_size, 10])
-    
-    # Convolution layer weights
-    W_conv1 = tf.Variable(tf.truncated_normal([3,3,3,64], stddev=5e-2))
-    b_conv1 = bias_variable([64])
-    W_conv2 = tf.Variable(tf.truncated_normal([3,3,64,64], stddev=5e-2))
-    b_conv2 = bias_variable([64])
-    W_conv3 = tf.Variable(tf.truncated_normal([3,3,64,64], stddev=5e-2))
-    b_conv3 = bias_variable([64])
-    W_conv4 = tf.Variable(tf.truncated_normal([3,3,64,64], stddev=5e-2))
-    b_conv4 = bias_variable([64])
-    W_conv5 = tf.Variable(tf.truncated_normal([3,3,64,64], stddev=5e-2))
-    b_conv5 = bias_variable([64])
-    W_conv6 = tf.Variable(tf.truncated_normal([3,3,64,64], stddev=5e-2))
-    b_conv6 = bias_variable([64])
-    
-    # Fully connected layers
-    flat_length = 8*8*64
-    W_fc1 = tf.Variable(tf.truncated_normal([flat_length, 384], stddev=0.004))
-    b_fc1 = bias_variable([384])
-    W_fc2 = tf.Variable(tf.truncated_normal([384, 192], stddev=0.004))
-    b_fc2 = bias_variable([192])
-    W_fc3 = tf.Variable(tf.truncated_normal([192, 10], stddev=1/192.0))
-    b_fc3 = bias_variable([10])
-    
-    # Reshape image
-    x_image = tf.reshape(x, [batch_size,32,32,3])
-    
-    # Forward pass - CNN
-    conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    #conv2 = tf.nn.relu(conv2d(conv1, W_conv2) + b_conv2)
-    #conv3 = tf.nn.relu(conv2d(conv1, W_conv3) + b_conv3)
-    pool1 = max_pool_3x3(conv1)
-    norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
-    conv4 = tf.nn.relu(conv2d(norm1, W_conv4) + b_conv4)
-    #conv5 = tf.nn.relu(conv2d(conv4, W_conv5) + b_conv5)
-    #conv6 = tf.nn.relu(conv2d(conv4, W_conv6) + b_conv6)
-    norm2 = tf.nn.lrn(conv4, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
-    pool2 = max_pool_3x3(norm2)
-    
-    # Forward pass - Fully Connected layer
-    pool2_flat = tf.reshape(pool2, [batch_size, flat_length])
-    fc1 = tf.nn.relu(tf.matmul(pool2_flat, W_fc1) + b_fc1)
-    fc2 = tf.nn.relu(tf.matmul(fc1, W_fc2) + b_fc2)
-    logits = tf.nn.relu(tf.matmul(fc2, W_fc3) + b_fc3)
+    x = tf.placeholder(tf.float32, [None, 3072])
+    y = tf.placeholder(tf.float32, [None, 10])
 
-    # Sum of activations of all hidden units
-    total_activation = tf.Variable(tf.zeros([batch_size]))
-    total_activation += tf.reduce_sum(fc1, 1)
-    total_activation += tf.reduce_sum(fc2, 1)
+    x_image = tf.reshape(x, [-1, 32, 32, 3])
 
-        
+    net = tflearn.conv_2d(x_image, 96, 3, 1, 'same', 'relu', weights_init=tflearn.initializations.xavier(),
+                          bias_init='uniform')
+    net = tflearn.conv_2d(net, 96, 3, 1, 'same', 'relu', weights_init=tflearn.initializations.xavier(),
+                          bias_init='uniform')
+    net = tflearn.conv_2d(net, 96, 3, 2, 'same', 'relu', weights_init=tflearn.initializations.xavier(),
+                          bias_init='uniform')
+    net = tflearn.conv_2d(net, 192, 3, 1, 'same', 'relu', weights_init=tflearn.initializations.xavier(),
+                          bias_init='uniform')
+    net = tflearn.conv_2d(net, 192, 3, 1, 'same', 'relu', weights_init=tflearn.initializations.xavier(),
+                          bias_init='uniform')
+    net = tflearn.conv_2d(net, 192, 3, 2, 'same', 'relu', weights_init=tflearn.initializations.xavier(),
+                          bias_init='uniform')
+    net = tflearn.conv_2d(net, 192, 3, 1, 'same', 'relu', weights_init=tflearn.initializations.xavier(),
+                          bias_init='uniform')
+    net = tflearn.conv_2d(net, 192, 1, 1, 'same', 'relu', weights_init=tflearn.initializations.xavier(),
+                          bias_init='uniform')
+    net = tflearn.conv_2d(net, 10, 1, 1, 'same', 'relu', weights_init=tflearn.initializations.xavier(),
+                          bias_init='uniform')
+    net = tflearn.global_avg_pool(net)
+
     # Calculate loss
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, y)
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(net, y)
     loss = tf.reduce_mean(cross_entropy)
-    
+
     # Find all the correctly classified examples
-    correct_ = tf.equal(tf.argmax(y,1), tf.argmax(logits,1))
-    
+    correct_ = tf.equal(tf.argmax(y, 1), tf.argmax(net, 1))
+
     # Optimizer with gradient clipping
     global_step = tf.Variable(0)
-    lr = tf.train.exponential_decay(0.1,global_step,40000,0.1,True)
+    lr = tf.train.exponential_decay(0.1, global_step, 40000, 0.1, True)
     optimizer = tf.train.GradientDescentOptimizer(lr)
     gradients, v = zip(*optimizer.compute_gradients(loss))
     gradients, _ = tf.clip_by_global_norm(gradients, 1.25)
     optimizer = optimizer.apply_gradients(zip(gradients, v), global_step=global_step)
-    
-    # Validation test
-    x_v = tf.placeholder(tf.float32, [100, 3072])
-    y_v = tf.placeholder(tf.float32, [100, 10])
-    
-    x_image_v = tf.reshape(x_v, [100,32,32,3])
-    # Forward propagation
-    # Forward pass - CNN
-    conv1_v = tf.nn.relu(conv2d(x_image_v, W_conv1) + b_conv1)
-    #conv2_v = tf.nn.relu(conv2d(conv1_v, W_conv2) + b_conv2)
-    #conv3_v = tf.nn.relu(conv2d(conv1_v, W_conv3) + b_conv3)
-    pool1_v = max_pool_3x3(conv1_v)
-    norm1_v = tf.nn.lrn(pool1_v, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
-    conv4_v = tf.nn.relu(conv2d(norm1_v, W_conv4) + b_conv4)
-    #conv5_v = tf.nn.relu(conv2d(conv4_v, W_conv5) + b_conv5)
-    #conv6_v = tf.nn.relu(conv2d(conv4_v, W_conv6) + b_conv6)
-    norm2 = tf.nn.lrn(conv4_v, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
-    pool2_v = max_pool_3x3(norm2)
 
-    # Forward pass - Fully Connected layer
-    pool2_flat_v = tf.reshape(pool2_v, [batch_size, flat_length])
-    fc1_v = tf.nn.relu(tf.matmul(pool2_flat_v, W_fc1) + b_fc1)
-    fc2_v = tf.nn.relu(tf.matmul(fc1_v, W_fc2) + b_fc2)
-    logits_v = tf.nn.relu(tf.matmul(fc2_v, W_fc3) + b_fc3)
-    
-    # Calculate accuracy
-    correct_prediction = tf.equal(tf.argmax(y_v,1), tf.argmax(logits_v,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    
-
-
-# Read data
-# Use first 4 data files as training data and last one as validation
+# ### Read data
+# * Use first 4 data files as training data and last one as validation
 
 train_x = []
 train_y = []
-for i in xrange(1,5):
-    dict_ = unpickle('../cifar-10-batches-py/data_batch_'+str(i))
+for i in xrange(1, 5):
+    dict_ = unpickle('cifar-10-batches-py/data_batch_' + str(i))
     if i == 1:
         train_x = dict_['data']
         train_y = np.eye(10)[dict_['labels']]
@@ -141,14 +71,15 @@ for i in xrange(1,5):
         train_x = np.concatenate((train_x, dict_['data']), axis=0)
         train_y = np.concatenate((train_y, np.eye(10)[dict_['labels']]), axis=0)
 
-dict_ = unpickle('../cifar-10-batches-py/data_batch_5')
+dict_ = unpickle('cifar-10-batches-py/data_batch_5')
 valid_x = dict_['data']
 valid_y = np.eye(10)[dict_['labels']]
 del dict_
 
-
 epochs = 150
-losses = []; accuracies=[]
+losses = []
+activations = []
+iterations = [0] * 40000
 
 with tf.Session(graph=graph) as session:
     tf.initialize_all_variables().run()
@@ -159,38 +90,43 @@ with tf.Session(graph=graph) as session:
         sequence = np.random.randint(len(train_x), size=len(train_x))
         random_train_x = train_x[sequence]
         random_train_y = train_y[sequence]
-        batch_xs = random_train_x[cursor:(cursor+batch_size)]
-        batch_ys = random_train_y[cursor:(cursor+batch_size)]
+        batch_xs = random_train_x[cursor: min((cursor + batch_size), len(train_x))]
+        batch_ys = random_train_y[cursor: min((cursor + batch_size), len(train_x))]
         feed_dict = {x: batch_xs, y: batch_ys}
-        _ = session.run([optimizer], feed_dict = feed_dict)
-        
+        _ = session.run([optimizer], feed_dict=feed_dict)
+
         cursor = (cursor + batch_size) % 40000
-
-        # If epoch is done
         if cursor == 0:
-            l_list = [];
-            print "GETTING LOSSES FOR TRAIN SET"
-            for iii in xrange(len(train_x)/batch_size):
-                batch_xs = train_x[iii*batch_size:(iii + 1)*batch_size]
-                batch_ys = train_y[iii*batch_size:(iii + 1)*batch_size]
+            l_list = []
+            ac_list = []
+            print "GETTING ACTIVATIONS, ITERATIONS AND LOSSES FOR ALL EXAMPLES"
+            for iii in xrange(400):
+                batch_xs = train_x[iii * 100: (iii + 1) * 100]
+                batch_ys = train_y[iii * 100: (iii + 1) * 100]
                 feed_dict = {x: batch_xs, y: batch_ys}
-                cr = session.run([cross_entropy], feed_dict=feed_dict)
+                cr, co = session.run([cross_entropy, correct_], feed_dict=feed_dict)
 
+                # Update iterations
+                for j in xrange(len(co)):
+                    if not co[j]:
+                        iterations[cursor + j] += 1
                 # Append losses, activations for batch
-                l_list.extend(cr[0]);
+                l_list.extend(cr)
+                ac_list.extend(co)
             # Append losses, activations for epoch
-            losses.append(l_list)
-            print "MEAN LOSS = "+str(np.mean(l_list))
+            losses.append(l_list)  # activations.append(ac_list)
+
             # Validation test
             print "TESTING ON VALIDATION SET for epoch = " + str(i)
             cor_pred = []
             for iii in xrange(100):
-                a = session.run([correct_prediction], feed_dict={x_v: valid_x[iii * 100:(iii + 1) * 100],
-                                                                 y_v: valid_y[iii * 100:(iii + 1) * 100]})
+                a = session.run([correct_], feed_dict={x: valid_x[iii * 100:(iii + 1) * 100],
+                                                       y: valid_y[iii * 100:(iii + 1) * 100]})
                 cor_pred.append(a)
             print "Accuracy = " + str(np.mean(cor_pred))
-            accuracies.append(np.mean(cor_pred))
-
             i += 1
-    np.save('shuffle-losses', losses)
-    np.save('shuffle-accuracies', accuracies)
+    losses = np.array(losses)
+    iterations = np.array(iterations)  # activations = np.array(activations)
+    print losses.shape, iterations.shape
+    np.save('iterations-all-conv-1', iterations)
+    np.save('losses-all-conv-1', losses)
