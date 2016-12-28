8413af53e54051e57ab98ed0a9fa7a279bc9f5ad
tomard <tomard@usi.ch>
2016-12-28 16:47:51 +0100

    only 500 steps now

--- expensive/cifar-10/master.py
+++ expensive/cifar-10/master.py
@@ -105,7 +105,7 @@ valid_x = np.array(dict_['data'])/255.0
 valid_y = np.eye(10)[dict_['labels']]
 del dict_
 
-epochs = 10 * int(round(40000/batch_size)+1)
+epochs = 500  # 10 * int(round(40000/batch_size)+1)
 losses = []
 selected_batches = []
 with tf.Session(graph=graph) as session:
