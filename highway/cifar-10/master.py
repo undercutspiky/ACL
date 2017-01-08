905e9534cf752259dd83e40c5892642682014a42
tomard <tomard@usi.ch>
2017-01-08 02:46:40 +0100

    divide graph among GPUs

--- highway/cifar-10/master.py
+++ highway/cifar-10/master.py
@@ -53,36 +53,44 @@ with graph.as_default():
     net = tflearn.batch_normalization(net)
     net = tf.nn.relu(net)
 
-    for ii in xrange(5):
-        net, t_s = conv_highway(net, 64, 64, 1, 3)
-        transform_sum += t_s
+    with tf.device("/gpu:0"):
+        for ii in xrange(5):
+            net, t_s = conv_highway(net, 64, 64, 1, 3)
+            transform_sum += t_s
 
-    net, t_s = conv_highway(net, 64, 128, 2, 3)
-    transform_sum += t_s
-    
-    for ii in xrange(7):
-        net, t_s = conv_highway(net, 128, 128, 1, 3)
-        transform_sum += t_s
-
-    net, t_s = conv_highway(net, 128, 256, 2, 3)
+    with tf.device("/gpu:0"):
+        net, t_s = conv_highway(net, 64, 128, 2, 3)
     transform_sum += t_s
 
-    for ii in xrange(11):
-        net, t_s = conv_highway(net, 256, 256, 1, 3)
-        transform_sum += t_s
+    with tf.device("/gpu:1"):
+        for ii in xrange(7):
+            net, t_s = conv_highway(net, 128, 128, 1, 3)
+            transform_sum += t_s
 
-    net, t_s = conv_highway(net, 256, 512, 2, 3)
+    with tf.device("/gpu:1"):
+        net, t_s = conv_highway(net, 128, 256, 2, 3)
     transform_sum += t_s
 
-    for ii in xrange(5):
-        net, t_s = conv_highway(net, 512, 512, 1, 3)
-        transform_sum += t_s
+    with tf.device("/gpu:2"):
+        for ii in xrange(11):
+            net, t_s = conv_highway(net, 256, 256, 1, 3)
+            transform_sum += t_s
 
-    net = tflearn.conv_2d(net, 10, 1, 1, 'same', 'linear', weights_init=tflearn.initializations.xavier(),
-                          bias_init='uniform', regularizer='L2')
-    net = tflearn.batch_normalization(net)
-    net = tf.nn.relu(net)
-    net = tflearn.global_avg_pool(net)
+    with tf.device("/gpu:2"):
+        net, t_s = conv_highway(net, 256, 512, 2, 3)
+    transform_sum += t_s
+
+    with tf.device("/gpu:3"):
+        for ii in xrange(5):
+            net, t_s = conv_highway(net, 512, 512, 1, 3)
+            transform_sum += t_s
+
+    with tf.device("/gpu:3"):
+        net = tflearn.conv_2d(net, 10, 1, 1, 'same', 'linear', weights_init=tflearn.initializations.xavier(),
+                              bias_init='uniform', regularizer='L2')
+        net = tflearn.batch_normalization(net)
+        net = tf.nn.relu(net)
+        net = tflearn.global_avg_pool(net)
 
     # Calculate loss
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(net, y)
