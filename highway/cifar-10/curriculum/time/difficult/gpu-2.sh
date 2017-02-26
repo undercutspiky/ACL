4ca7150410dbb7cf5c70c8e6d291edb3e469b200
tomard <tomard@usi.ch>
2017-02-26 12:02:31 +0100

    curriculums

---
+++ highway/cifar-10/curriculum/time/difficult/gpu-2.sh
@@ -0,0 +1,15 @@
+cd 1-epoch/run-2/
+python master.py 1
+python random-master.py 1
+cd ../../2-epochs/run-2
+python master.py 1
+python random-master.py 1
+cd ../../3-epochs/run-2
+python master.py 1
+python random-master.py 1
+cd ../../4-epochs/run-2
+python master.py 1
+python random-master.py 1
+cd ../../5-epochs/run-2
+python master.py 1
+python random-master.py 1
