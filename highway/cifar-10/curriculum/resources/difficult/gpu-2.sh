3d49111a9c9eb706e77ca16c6e5526409d3bca1b
Dhananjay Tomar <undercutspiky@yahoo.co.in>
2017-02-27 12:34:01 +0100

    results and shit

--- highway/cifar-10/curriculum/resources/difficult/gpu-2.sh
+++ highway/cifar-10/curriculum/resources/difficult/gpu-2.sh
@@ -1,15 +1,6 @@
 cd 1-epoch/run-2/
-python master.py 1
 python random-master.py 1
 cd ../../2-epochs/run-2
-python master.py 1
 python random-master.py 1
 cd ../../3-epochs/run-2
-python master.py 1
-python random-master.py 1
-cd ../../4-epochs/run-2
-python master.py 1
-python random-master.py 1
-cd ../../5-epochs/run-2
-python master.py 1
 python random-master.py 1
