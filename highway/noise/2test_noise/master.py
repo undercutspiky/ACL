1a6b3681d62e666107164491a53478de9384c29b
Dhananjay Tomar <undercutspiky@yahoo.co.in>
2017-05-25 00:57:02 +0200

    width 2 highway net trained without noise

--- highway/noise/2test_noise/master.py
+++ highway/noise/2test_noise/master.py
@@ -62,7 +62,7 @@ def conv_highway(x, fan_in, fan_out, stride, filter_size, not_pool=False):
     return (res + (C * x)), tf.reduce_sum(T, axis=[1,2,3])
 
 batch_size = 128
-width = 1
+width = 2
 
 graph = tf.Graph()
 with graph.as_default():
